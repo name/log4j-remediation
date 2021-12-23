93c38f0219c88ff76db3e8b00b24ebf49a5149e3
charlie <charlie@maddex.co>
2021-12-23 23:10:14 +0000

    feat: discover vulnerable files
    Signed-off-by: charlie <charlie@maddex.co>

--- backend/discover.go
+++ backend/discover.go
@@ -1,6 +1,40 @@
 package backend
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/schollz/progressbar/v3"
+	"github.com/shirou/gopsutil/disk"
+)
+
 // Go : Starts discovery of '.ear|.jar|.war' files
 func Go() {
+	var detections []string
+	partitions, _ := disk.Partitions(true)
+	for _, partition := range partitions {
+		log.Println("Discovering on " + partition.Mountpoint)
+		bar := progressbar.NewOptions(-1,
+			progressbar.OptionSetDescription("Discovering files"),
+			progressbar.OptionShowCount(),
+			progressbar.OptionSpinnerType(40),
+		)
+		e := filepath.Walk(partition.Mountpoint+"/", func(path string, f os.FileInfo, err error) error {
+			bar.Add(1)
+			if strings.Contains(path, "log4j-core-") && !strings.Contains(path, "2.17") {
+				if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".ear") || strings.HasSuffix(path, ".war") {
+					detections = append(detections, path)
+				}
+			}
+			return nil
+		})
 
+		if e != nil {
+			log.Println(e)
+		}
+		fmt.Println()
+	}
 }
--- go.mod
+++ go.mod
@@ -4,13 +4,16 @@ go 1.17
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/schollz/progressbar/v3 v3.8.5 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/urfave/cli v1.22.5 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
